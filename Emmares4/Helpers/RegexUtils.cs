31d9ff6807a679cc048d200f4b3686496d90cec1
Janko <jankojovicic351@gmail.com>
2021-02-09 15:01:10 +0100

    Adding the new classes

---
+++ Emmares4/Helpers/RegexUtils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Emmares4.Helpers
+{
+    public class RegexUtils
+    {
+        // A regular expression for validating slugs.
+        // Does not allow leading or trailing hypens or whitespace.
+        public static readonly Regex SlugRegex = new Regex(@"(^[a-z0-9])([a-z0-9_-]+)*([a-z0-9])$");
+
+    }
+}
